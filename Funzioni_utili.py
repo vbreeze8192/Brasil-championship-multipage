e8459f54a73e13b6c810ab3a0e9adb2457ac5c2d
Veronica <ve.brizzi@gmail.com>
2023-06-23 23:31:44 +0200

    Update Funzioni_utili.py

--- Funzioni_utili.py
+++ Funzioni_utili.py
@@ -289,12 +289,14 @@ def doyourstupidthings(name,year_col,col_day,anni,anno_val,day='NA',what='pred',
     squadre=list(raw.groupby(['SQUADRA']).mean().index)
 
     raw['HOUR']=0
+    st.write(raw[col_time])
+    st.write(type(raw[col_time])
     try:
         raw[col_time]=pd.to_datetime(raw[col_time])
     except Exception as e:
          pass
-    for ii in raw.index:
-        raw['HOUR'].iloc[ii]=int(pd.to_datetime(raw[col_time].iloc[ii]).hour)
+    for ii in range(0,len(raw)):
+        raw['HOUR'].iloc[ii]=int(raw[col_time].iloc[ii].hour)
     '''
     for col in [col_hg,col_ag,col_res]:
         raw[col]=0
