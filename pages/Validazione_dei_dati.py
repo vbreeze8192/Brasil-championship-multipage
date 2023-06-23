e54767222a6c58a6f2364b0221decf289775a937
Veronica <ve.brizzi@gmail.com>
2023-06-23 10:32:59 +0200

    Update Validazione_dei_dati.py

--- pages/Validazione_dei_dati.py
+++ pages/Validazione_dei_dati.py
@@ -80,10 +80,10 @@ if st.button('Prevedi for Braaasil',disabled=not uploaded_file, type='primary'):
     for output in outputs:
         st.write('Confusion matrix per primo algoritmo')
         [input,input_lower]=starting()
-        cm = confusion_matrix(val_df[output_choice], alg_w(val_df[input]))
+        cm = confusion_matrix(val_df[output_choice], alg_w.predict(val_df[input]))
         st.write(cm)
         st.write('Confusion matrix per secondo algoritmo')
-        cm = confusion_matrix(val_df[output_choice], alg_lp(val_df[input_lower]))
+        cm = confusion_matrix(val_df[output_choice], alg_lp.predict(val_df[input_lower]))
         st.write(cm)
 
   
