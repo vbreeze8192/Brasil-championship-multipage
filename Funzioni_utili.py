2a3de2d89991f00e95cfbe80f44e3c6ef8a0eab1
Veronica <ve.brizzi@gmail.com>
2023-06-24 00:29:56 +0200

    Update Funzioni_utili.py

--- Funzioni_utili.py
+++ Funzioni_utili.py
@@ -388,7 +388,9 @@ def doyourstupidthings(name,year_col,col_day,anni,anno_val,day='NA',what='pred',
             squadre_day=list(df_period.groupby(['SQUADRA']).mean().index)
             [avgnowch,avgdxdnowch]=champions_metrics(df_period,col_day=col_day)
             [ndnows,qtymaxnows]=team_metrics(df_period,squadre)
-
+            st.write(squadre_day)
+            st.write(df_period)
+            
             for squadra in squadre_day:
                 line_df=df_period[df_period[col_day]==day_iter]
                 line_team=line_df[line_df['SQUADRA']==squadra]
