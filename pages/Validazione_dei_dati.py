fcddb1b3b9460a2ec94cf899b1c2072a0a7cfcf3
Veronica <ve.brizzi@gmail.com>
2023-06-23 12:27:51 +0200

    Update Validazione_dei_dati.py

--- pages/Validazione_dei_dati.py
+++ pages/Validazione_dei_dati.py
@@ -67,11 +67,11 @@ if st.button('Prevedi for Braaasil',disabled=not uploaded_file, type='primary'):
     
     [raw,final_df,int_df]=doyourstupidthings(uploaded_file,year_col,col_day,anni,anno_val,what='val')
     [final_df,alg_w,alg_lp]=prediction(output_choice,final_df)
-    squadre=list(raw.groupby(['SQUADRA']).mean().index)
+    squadre=list(final_df.groupby(['SQUADRA']).mean().index)
     val_df=final_df.copy()
     for squadra in squadre:
         #Pari nelle prossime N partite da D=now a D=now+N
-        temp=raw[raw['SQUADRA']==squadra]
+        temp=final_df[final_df['SQUADRA']==squadra]
         (temp,outputs)=d_in_future(temp,4)
         val_df=pd.concat([val_df,temp])
     st.write('Ecco i dati completi')
