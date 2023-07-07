9275ecdfee1d3975ca7d3aaca757973b4642a909
Veronica <ve.brizzi@gmail.com>
2023-07-07 08:53:19 +0200

    Update Validazione_dei_dati.py

--- pages/Validazione_dei_dati.py
+++ pages/Validazione_dei_dati.py
@@ -99,8 +99,8 @@ if st.button('Prevedi for Braaasil',disabled=not(uploaded_file and uploaded_mode
 
     # visualize the first prediction's explanation 
     shap.initjs()
-    shap.force_plot(explainer.expected_value, shap_values, val_df[input])
-    st.pyplot()
+    test=shap.force_plot(explainer.expected_value, shap_values, val_df[input])
+    st.write(test)
 
 
   
